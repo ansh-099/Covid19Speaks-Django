b24a59f2250b5e014ae6f2232b90c12664fcce17
ansh-099 <ansh.099@gmail.com>
2020-07-19 16:11:45 +0530

    static files

--- covid19speaks/settings.py
+++ covid19speaks/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
@@ -132,4 +135,5 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
 
